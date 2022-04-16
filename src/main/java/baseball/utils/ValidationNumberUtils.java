7afd29db57fb6cc88b5e9b0e118c7b5cd055aa0f
msm <msmasd@naver.com>
2022-04-17 02:04:58 +0900

    feat: 세자리 숫자에서 중복된 숫자에 대한 유효성 검증 함수 생성

--- src/main/java/baseball/utils/ValidationNumberUtils.java
+++ src/main/java/baseball/utils/ValidationNumberUtils.java
@@ -1,5 +1,9 @@
 package baseball.utils;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ValidationNumberUtils {
 
     public static final int MIN_NUMBER = 1;
@@ -8,6 +12,14 @@ public class ValidationNumberUtils {
     public static void validateRange(int number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException("cannot over " + MAX_NUMBER + " and less " + MIN_NUMBER);
+        }
+    }
+
+    public static void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+
+        if (numbers.size() != numberSet.size()) {
+            throw new IllegalArgumentException("cannot duplicate");
         }
     }
 }
--- src/test/java/baseball/utils/ValidationNumberUtilsTest.java
+++ src/test/java/baseball/utils/ValidationNumberUtilsTest.java
@@ -2,7 +2,8 @@ package baseball.utils;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class ValidationNumberUtilsTest {
@@ -19,5 +20,14 @@ public class ValidationNumberUtilsTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> ValidationNumberUtils.validateRange(10))
                 .withMessage(exceptionMessage);
+    }
+
+    @Test
+    void 세자리_숫자에서_중복된_숫자에_대한_유효성_검증() {
+        ValidationNumberUtils.validateDuplicate(Arrays.asList(1, 2, 3));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ValidationNumberUtils.validateDuplicate(Arrays.asList(1, 2, 1)))
+                .withMessage("cannot duplicate");
     }
 }
